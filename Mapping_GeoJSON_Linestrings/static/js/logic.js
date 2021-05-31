751fab3edb84365e71ad63ba08171b7983c2aca7
Tucker Fischer <tucker.j.fischer@gmail.com>
2021-05-31 15:43:00 -0500

    Linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,4 +1,4 @@
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 7,
     accessToken: API_KEY
@@ -11,21 +11,21 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 let baseMaps = {
-  Light: light,
+  Street: streets,
   Dark: dark
 };
 
 let map = L.map('mapid', {
-  center: [44.0, -80.0],
+  center: [30, 30],
   zoom: 2,
-  layers: [light]
+  layers: [streets]
 })
 
 L.control.layers(baseMaps).addTo(map);
 
-let torontoData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoRoutes.json";
+let airportData = "https://raw.githubusercontent.com/tuckerfischer/Mapping_Earthquakes/main/majorAirports.json";
 
-d3.json(torontoData).then(function(data) {
+d3.json(airportData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data).addTo(map);
